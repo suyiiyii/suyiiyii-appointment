18a09b2ea863598fc4f88b9bad7574d23691068f
suyiiyii <suyiiyii@gmail.com>
2024-03-04 18:18:26 +0800

    后端架构初始化

---
+++ backend/app/main.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from .routers import users
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(users.router)
+
+
---
+++ backend/app/routers/users.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+from pydantic import BaseModel
+import jwt
+from ..dependencies import SECRET_KEY
+from ..dependencies import ALGORITHM
+from datetime import timedelta
+import time
+
+router = APIRouter(prefix='/users', tags=['users'])
+
+
+users_sheet = [{"username": "su", "password": "qwe"}]
+
+
+@router.get('/', tags=["users"])
+async def read_users():
+    return users_sheet
+
+
+
+class User_login(BaseModel):
+    username: str
+    password: str
+
+
+@router.post('/login')
+def login(user: User_login):
+    username = user.username
+    password = user.password
+    for user in users_sheet:
+        if user['username'] == username and user['password'] == password:
+            experation_time = time.time() + timedelta(days=1).total_seconds()
+            token_data = {"sub": user["username"],"exp": experation_time}
+            token = jwt.encode(token_data, SECRET_KEY, ALGORITHM)
+            return {
+                "message": "login success",
+                "token": {"access_token": token, "token_type": "bearer"},
+            }
+
+    return {"message": "username or passwrod is incorrect"}
