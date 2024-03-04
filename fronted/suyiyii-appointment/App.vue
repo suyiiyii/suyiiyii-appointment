b89b486cea8da1846da9293da2e04046b13b877e
suyiiyii <suyiiyii@gmail.com>
2024-03-04 10:54:26 +0800

    导入前端项目

---
+++ fronted/suyiyii-appointment/App.vue
@@ -0,0 +1,25 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+
+	.content {
+		margin-top: 3em;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
---
+++ fronted/suyiyii-appointment/pages/index/appointment.vue
@@ -0,0 +1,23 @@
+<template>
+	<view class="content">
+		<h1>这是预约界面</h1>
+		<image src="https://via.placeholder.com/400x200.png/3c9cff/fff"></image>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
