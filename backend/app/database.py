f70dd2df31afe915aef2f64c9b928356564bb010
suyiiyii <suyiiyii@gmail.com>
2024-03-07 00:20:30 +0800

    fix db bug

--- backend/app/database.py
+++ backend/app/database.py
@@ -8,9 +8,7 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
