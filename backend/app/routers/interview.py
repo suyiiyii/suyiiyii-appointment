218683e48702eaa143ce68157eb17606d37033be
suyiiyii <suyiiyii@gmail.com>
2024-03-10 16:55:43 +0800

    temp_test

--- backend/app/routers/interview.py
+++ backend/app/routers/interview.py
@@ -31,11 +31,11 @@ def register_interview_session(
     db: Session = Depends(get_db),
     current_user: schemas.UserInDB = Depends(get_current_user),
 ):
-    # 检查是否已经注册
-    if crud.read_interview_session(db, session_name):
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="Session already registered"
-        )
+    # # 检查是否已经注册
+    # if crud.read_interview_session(db, session_name):
+    #     raise HTTPException(
+    #         status_code=status.HTTP_400_BAD_REQUEST, detail="Session already registered"
+    #     )
     crud.add_interview_session_relation(db, current_user.username, session_name)
     return session_name
 
